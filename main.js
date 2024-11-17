17cac5ceecfc06405672ffa9984e005a6093f14f
Liang Zhang <liang@leon-MacBookAir>
2024-11-16 16:32:42 -0800

    add youtube iframe and make media player play videos

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import config from './config.js';
 const audioPlayer = document.querySelector("#audioPlayer");
+const youtubeIframe = document.querySelector("#youtubeIframe");
 audioPlayer.setAttribute("src", config.audioUrl);
 
 console.log(audioPlayer);
+
+function resetAudioPlayerUrl(url) {
+    audioPlayer.setAttribute("src", url);
+}
 
 function togglePlayPause() {
     if (audioPlayer.paused) {
@@ -20,6 +25,10 @@ function forward(seconds) {
     audioPlayer.currentTime = Math.max(0, audioPlayer.currentTime + seconds);
 }
 
+function resetYoutubeUrl(url) {
+    youtubeIframe.setAttribute("src", url);
+}
+
 document.addEventListener('keydown', (event) => {
     if (event.ctrlKey && event.code === 'KeyP') {
         event.preventDefault();
@@ -32,3 +41,23 @@ document.addEventListener('keydown', (event) => {
         forward(5);
     }
 });
+
+document.querySelector("#customAudioPlayer").addEventListener('click', () => {
+    let url = prompt("custom audio player url:");
+    resetAudioPlayerUrl(url);
+});
+
+document.querySelector("#customYoutube").addEventListener('click', () => {
+    let url = prompt("custom youtube url:");
+    resetYoutubeUrl(url);
+});
+
+
+document.querySelector("#focus").addEventListener('click', () => {
+    //alert(111);
+    let iframe = document.querySelector("iframe");
+    console.log(iframe);
+    console.log(iframe.contentDocument);
+    console.log(iframe.contentWindow.document);
+});
+
